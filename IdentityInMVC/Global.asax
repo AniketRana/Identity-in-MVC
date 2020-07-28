0821fe50cdfb9077af46311b5d6515f9f3078213
Aniketrana <ranaannu93@gmail.com>
2020-07-28 13:07:25 +0530

    Add project files.

---
+++ IdentityInMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityInMVC.MvcApplication" Language="C#" %>
---
+++ IdentityInMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityInMVC.Startup))]
+namespace IdentityInMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
